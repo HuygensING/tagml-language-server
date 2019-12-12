8fdb0dfd7a4ae9f45f39d2462e8c3f18ce9a57ab
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2019-12-12 17:56:07 +0100

    Try parsing urls according to W3C EBNF

--- src/main/kotlin/nl/knaw/huc/di/rd/tag/tagml/tokenizer/URLParser.kt
+++ src/main/kotlin/nl/knaw/huc/di/rd/tag/tagml/tokenizer/URLParser.kt
@@ -3,32 +3,51 @@ package nl.knaw.huc.di.rd.tag.tagml.tokenizer
 import lambdada.parsec.parser.*
 
 object URLParser {
-    val digit = charIn('0', '9')
+
+    private fun <A, B> Pair<A, B>.concatenateLeaves(): String {
+        val leftString = asString(first)
+        val rightString = asString(second)
+        return "$leftString$rightString"
+    }
+
+    private fun <A> asString(obj: A): String {
+        return when (obj) {
+            is Pair<*, *> -> obj.concatenateLeaves()
+            is List<*> -> obj.joinToString("") { asString(it) }
+            null -> ""
+            else -> obj.toString()
+        }
+    }
+
+    val digit = charIn(CharRange('0', '9'))
     val digits = digit.rep
     val port = digits
-    val lowalpha = charIn('a', 'z')
-    val highalpha = charIn('A', 'Z')
+    val lowalpha = charIn(CharRange('a', 'z'))
+    val highalpha = charIn(CharRange('A', 'Z'))
     val alpha = lowalpha or highalpha
     val alphadigit = alpha or digit
-    val domainlabel = alphadigit or (alphadigit then (alphadigit or char('-')).optrep then alphadigit)
+    val domainlabel = (alphadigit then (alphadigit or char('-')).optrep then alphadigit)
     val toplabel = alpha or (alpha then (alphadigit or char('-')).optrep then alphadigit)
     val hostname = (domainlabel then char('.')).optrep then toplabel
     val hostnumber = digits then char('.') then digits then char('.') then digits then char('.') then digits
     val host = hostname or hostnumber
     val hostport = host then (char(':') then port).opt
-    val hex = digit or charIn('a', 'f') or charIn('A', 'F')
+    val hex = digit or charIn(CharRange('a', 'f')) or charIn(CharRange('A', 'F'))
     val escape = char('%') then hex then hex
     val safe = charIn("""$-_.+""")
     val extra = charIn("""!*'(),""")
     val unreserved = alpha or digit or safe or extra
     val uchar = unreserved or escape
     val hsegment = (uchar or charIn(";:@&=")).optrep
-    val search = (uchar or charIn(";:@&=")).optrep
+    val search = hsegment
     val hpath = hsegment then (char('/') then hsegment).optrep
-    val httpurl = string("http") then char('s').opt then string("://") then hostport then (char('/') then hpath then (char('?') then search).opt).opt
-    val fsegment = (uchar or charIn(";:@&=")).optrep
+    val httpurl = (string("http") then char('s').opt then string("://") then hostport then (char('/') then hpath then (char('?') then search).opt).opt)
+            .map { it.concatenateLeaves() }
+
+    val fsegment = hsegment
     val fpath = fsegment then (char('/') then fsegment).optrep
-    val fileurl = string("file://") then (host or string("localhost")).opt then char('/') then fpath
+    val fileurl = (string("file://") then (host or string("localhost")).opt then char('/') then fpath)
+            .map { it.concatenateLeaves() }
 
     val url = httpurl or fileurl
 
--- src/test/kotlin/nl/knaw/huc/di/rd/tag/tagml/tokenizer/URLParserTest.kt
+++ src/test/kotlin/nl/knaw/huc/di/rd/tag/tagml/tokenizer/URLParserTest.kt
@@ -1,20 +1,67 @@
 package nl.knaw.huc.di.rd.tag.tagml.tokenizer
 
 import lambdada.parsec.io.Reader
+import lambdada.parsec.parser.*
+import mu.KotlinLogging
+import nl.knaw.huc.di.rd.tag.tagml.tokenizer.URLParser.digit
+import nl.knaw.huc.di.rd.tag.tagml.tokenizer.URLParser.digits
+import nl.knaw.huc.di.rd.tag.tagml.tokenizer.URLParser.domainlabel
+import nl.knaw.huc.di.rd.tag.tagml.tokenizer.URLParser.hostname
+import nl.knaw.huc.di.rd.tag.tagml.tokenizer.URLParser.hostport
 import nl.knaw.huc.di.rd.tag.tagml.tokenizer.URLParser.url
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class URLParserTest() {
+    private val logger = KotlinLogging.logger {}
+
     @Test
-    fun testURLs() {
-        assertURLParses("http://example.org")
+    fun testABBA() {
+        val p = char('a') then charIn("ab").optrep then char('a')
+        assertParses("aa", p)
+        assertParses("aba", p)
+        assertParses("abba", p)
+        assertParses("ababa", p)
+        assertDoesNotParse("ababab", p)
     }
 
-    private fun assertURLParses(urlString: String) {
-        val urlReader = Reader.string(urlString)
-        val response = url(urlReader)
-        when (response) {
 
-        }
+    @Test
+    fun test1() {
+        assertParses("0", digit)
+        assertParses("1", digit)
+        assertParses("9", digit)
+        assertDoesNotParse("X", digit)
+        assertParses("123", digits)
+        assertParses("example", domainlabel)
+        assertParses("example.org", hostname)
+        assertParses("example.org", hostport)
+    }
+
+    @Test
+    fun testURLs() {
+        assertParses("http://example.org", url)
+        assertParses("http://example.org/schema.yaml", url)
+        assertParses("http://example.org/schema?format=yaml", url)
+        assertParses("https://example.org/schema.yaml", url)
+        assertParses("file:///home/user/schema.yaml", url)
+    }
+
+    private fun <A> assertParses(string: String, p: Parser<Char, A>) {
+        val response = parse<A>(string, p)
+        assertThat(response.isSuccess()).isTrue()
+    }
+
+    private fun <A> assertDoesNotParse(string: String, p: Parser<Char, A>) {
+        val response = parse(string, p)
+        assertThat(response.isSuccess()).isFalse()
+    }
+
+    private fun <A> parse(string: String, p: Parser<Char, A>): Response<Char, A> {
+        val parser = p thenLeft eos()
+        val reader = Reader.string(string)
+        val response = parser(reader)
+        logger.info("string='$string', response=${response}")
+        return response
     }
 }
