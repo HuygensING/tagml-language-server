cf1a422a39d6ec725de1f2b41182dbdc041a1070
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2020-02-05 13:52:40 +0100

    [TRD-458] extend TokenIndexTest

--- src/test/kotlin/nl/knaw/huc/di/rd/tag/tagml/tokenizer/TokenIndexTest.kt
+++ src/test/kotlin/nl/knaw/huc/di/rd/tag/tagml/tokenizer/TokenIndexTest.kt
@@ -28,10 +28,19 @@ class TokenIndexTest {
         assertThat(index.locatedTokens).containsExactly(lt1, lt2, lt3)
 
         assertThat(index.tokenAt(p(0, 0))).isEqualTo(lt1)
-        assertThat(index.tokenAt(p(1, 1))).isEqualTo(lt2)
-        assertThat(index.tokenAt(p(2, 2))).isEqualTo(lt2)
-        assertThat(index.tokenAt(p(3, 3))).isEqualTo(lt3)
+        assertThat(index.tokenAt(p(0, 3))).isEqualTo(lt1)
+        assertThat(index.tokenAt(p(0, 6))).isEqualTo(lt1)
         assertThat(index.tokenAt(p(0, 10))).isNull()
+
+        assertThat(index.tokenAt(p(1, 1))).isEqualTo(lt2)
+        assertThat(index.tokenAt(p(1, 100))).isEqualTo(lt2)
+        assertThat(index.tokenAt(p(2, 2))).isEqualTo(lt2)
+        assertThat(index.tokenAt(p(2, 200))).isNull()
+
+        assertThat(index.tokenAt(p(3, 0))).isEqualTo(lt3)
+        assertThat(index.tokenAt(p(3, 3))).isEqualTo(lt3)
+        assertThat(index.tokenAt(p(3, 6))).isEqualTo(lt3)
+
         assertThat(index.tokenAt(p(10, 10))).isNull()
     }
 
