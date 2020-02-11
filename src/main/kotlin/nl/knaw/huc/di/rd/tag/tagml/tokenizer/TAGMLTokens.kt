cb2a4c6a92681f7748a05156a0e5abae71b64b4e
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2020-02-11 15:23:44 +0100

    [TRD-458] adjust range: the end position is exclusive

--- src/main/kotlin/nl/knaw/huc/di/rd/tag/tagml/tokenizer/TAGMLTokens.kt
+++ src/main/kotlin/nl/knaw/huc/di/rd/tag/tagml/tokenizer/TAGMLTokens.kt
@@ -8,8 +8,12 @@ abstract class TAGMLToken {
 }
 
 data class LSPToken(val token: TAGMLToken, val range: Range) {
+    //    A range in a text document expressed as (zero-based) start and end positions.
+    //    A range is comparable to a selection in an editor. Therefore the end position is exclusive.
+    //    If you want to specify a range that contains a line including the line ending character(s)
+    //    then use an end position denoting the start of the next line.
     override fun toString(): String {
-        return "$token at [(${range.start.line},${range.start.character})-(${range.end.line},${range.end.character})]"
+        return "$token at [(${range.start.line},${range.start.character})-(${range.end.line},${range.end.character + 1})]"
     }
 }
 
